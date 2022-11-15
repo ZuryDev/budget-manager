c58de68a64ab617a544010c501bc35e437aaf462
ZuryDev <zurydevs@gmail.com>
2022-11-14 22:45:47 -0300

    create State

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,15 +1,36 @@
-import React, {Fragment} from 'react'
+import React, { Fragment, useState } from 'react'
 
 const Pregunta = () => {
+
+// definir el state
+const [ cantidad, guardarCantidad ] = useState(0);
+
+//funcion que lee presupuesto 
+const definirPresupuesto = e => {
+  guardarCantidad( parseInt(e.target.value, 10))
+}
+
+//sumit para definir el presupuesto
+const agregarPresupuesto = e => {
+  e.preventDefault(); 
+
+  //validar
+
+  //si se pasa la validacion
+}
+
   return (
     <Fragment>
         <h2> Coloca tu presupuesto</h2>
 
-        <form>
+        <form
+          onSubmit={agregarPresupuesto}
+        >
             <input
             type="number"
             className="u-full-width"
             placeholder="coloca tu presupuesto"
+            onChange={definirPresupuesto}
             />
 
         <input 
